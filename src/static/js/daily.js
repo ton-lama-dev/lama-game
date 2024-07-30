e9254f5bb1fa47c36d1c9535d0b0bdd20cfd62ee
lama-dev <techno.answer.io@gmail.com>
2024-07-30 17:00:27 +0300

    tasks page

--- src/static/js/daily.js
+++ src/static/js/daily.js
@@ -4,5 +4,9 @@ tg.BackButton.show()
 
 tg.onEvent('backButtonClicked', backCallback)
 function backCallback() {
-    window.location.href = "https://680b-217-25-86-62.ngrok-free.app/src/templates/";
+    window.location.href = "https://9e10-217-25-86-62.ngrok-free.app/src/templates/";
+};
+
+document.getElementById("tasks-btn").onclick = function() {
+    window.location.href = "https://9e10-217-25-86-62.ngrok-free.app/src/templates/tasks.html"
 };
--- src/static/js/index.js
+++ src/static/js/index.js
@@ -4,5 +4,5 @@ tg.BackButton.show()
 
 tg.onEvent('backButtonClicked', backCallback)
 function backCallback() {
-    window.location.href = "https://680b-217-25-86-62.ngrok-free.app/src/templates/";
+    window.location.href = "https://9e10-217-25-86-62.ngrok-free.app/src/templates/";
 };
