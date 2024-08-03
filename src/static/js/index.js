53aea7fdda196b492f7a8b1dc7c50189f8fa0c8b
lama-dev <techno.answer.io@gmail.com>
2024-08-03 17:12:07 +0300

    progress bar interactive

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,44 +1,67 @@
-let tg = window.Telegram.WebApp
-const $lama = document.getElementById("lama")
-const $balance = document.getElementById("balance")
-const $tasks = document.getElementById("tasks")
-const $upgrade = document.getElementById("upgrade")
-const $friends = document.getElementById("friends")
+let tg = window.Telegram.WebApp;
 
-tg.BackButton.show()
+const $lama = document.getElementById("lama");
+const $balance = document.getElementById("balance");
+const $tasks = document.getElementById("tasks");
+const $upgrade = document.getElementById("upgrade");
+const $friends = document.getElementById("friends");
+const $progressBar = document.getElementById("progress-bar");
+const $progressText = document.getElementById("progress-text");
+
+let availableEnergy = Number($progressText.textContent.split("/")[0]);
+let maxEnergy = Number($progressText.textContent.split("/")[1]);
+let tap = 10;
+let refill = 0.1;
+
+tg.BackButton.show();
 
 
 function increaseBalance(num) {
   $balance.textContent = Number($balance.textContent) + num;
-}
+
+  availableEnergy -= num;
+  newWidth = availableEnergy / maxEnergy * 100;
+  $progressBar.style.width = `${newWidth}%`;
+
+  $progressText.textContent = `${availableEnergy}/${maxEnergy}`
+};
+
+function decreaseBar(num) {
+
+};
 
 
 $lama.addEventListener("click", (event) => {
-  const rect = $lama.getBoundingClientRect();
-  const offsetX = event.clientX - rect.left - rect.width / 2;
-  const offsetY = event.clientY - rect.top - rect.height / 2;
-  const DEG = 60;
-  const tiltX = (offsetY / rect.height) * DEG;
-  const tiltY = (offsetX / rect.width) * -DEG;
-
-  $lama.style.setProperty("--tiltX", `${tiltX}deg`);
-  $lama.style.setProperty("--tiltY", `${tiltY}deg`);
-
-  setTimeout(() => {
-    $lama.style.setProperty("--tiltX", `0deg`);
-    $lama.style.setProperty("--tiltY", `0deg`);
-  }, 100);
-
-  const plusOne = document.createElement("div");
-  plusOne.classList.add("plus-one");
-  plusOne.textContent = "+1";
-  plusOne.style.left = `${event.clientX - rect.left + 30}px`;
-  plusOne.style.top = `${event.clientY - rect.top + 200}px`;
-  $lama.parentElement.appendChild(plusOne);
-  increaseBalance(1);
-  setTimeout(() => {
-    plusOne.remove()
-  }, 2000);
+  if (tap <= availableEnergy) {
+    const rect = $lama.getBoundingClientRect();
+    const offsetX = event.clientX - rect.left - rect.width / 2;
+    const offsetY = event.clientY - rect.top - rect.height / 2;
+    const DEG = 60;
+    const tiltX = (offsetY / rect.height) * DEG;
+    const tiltY = (offsetX / rect.width) * -DEG;
+  
+    $lama.style.setProperty("--tiltX", `${tiltX}deg`);
+    $lama.style.setProperty("--tiltY", `${tiltY}deg`);
+  
+    setTimeout(() => {
+      $lama.style.setProperty("--tiltX", `0deg`);
+      $lama.style.setProperty("--tiltY", `0deg`);
+    }, 100);
+  
+    const plusOne = document.createElement("div");
+    plusOne.classList.add("plus-one");
+    plusOne.textContent = "+1";
+    plusOne.style.left = `${event.clientX - rect.left + 30}px`;
+    plusOne.style.top = `${event.clientY - rect.top + 200}px`;
+    $lama.parentElement.appendChild(plusOne);
+    increaseBalance(tap);
+    decreaseBar(tap)
+    setTimeout(() => {
+      plusOne.remove()
+    }, 2000);
+  } else {
+    console.log("again  ")
+  };
 });
 
 
@@ -77,3 +100,6 @@ document.addEventListener('DOMContentLoaded', () => {
       updateProgress(500);
   }, 2000);
 });
+
+
+decreaseBar(1)
