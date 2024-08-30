8ed7a3fcb0cc8422b029162301b9cc699501590d
lama-dev <techno.answer.io@gmail.com>
2024-08-31 00:57:28 +0300

    referral link added

--- src/static/js/friends.js
+++ src/static/js/friends.js
@@ -1,11 +1,41 @@
-let tg = window.Telegram.WebApp;
-let user_tg_id = tg.initDataUnsafe.user.id;
+// let tg = window.Telegram.WebApp;
+// let user_tg_id = tg.initDataUnsafe.user.id;
+user_tg_id = 6257396100;
 
 const homeUrl = "https://9113-217-25-86-16.ngrok-free.app/"
 
-tg.BackButton.show()
+// tg.BackButton.show()
 
-tg.onEvent('backButtonClicked', backCallback)
-function backCallback() {
-    window.location.href = homeUrl + `main?user_id=${user_tg_id}`;
-};
+// tg.onEvent('backButtonClicked', backCallback)
+// function backCallback() {
+//     window.location.href = homeUrl + `main?user_id=${user_tg_id}`;
+// };
+
+document.addEventListener('DOMContentLoaded', function() {
+    const $refLink = document.getElementById("ref-link");
+    $refLink.innerText = `https://t.me/lama_airdrop_bot/game?startapp=${user_tg_id}`
+
+
+    const $copyBtn = document.getElementById("copy-btn");
+    $copyBtn.addEventListener('click', function() {
+        navigator.clipboard.writeText($refLink.innerText);
+        closeTapPopup();
+    });
+
+
+    const $inviteBtn = document.getElementById('invite-btn');
+    const $popup = document.getElementById('popup');
+    const $overlay = document.getElementById('overlay');
+
+    $inviteBtn.addEventListener('click', function() {
+        $overlay.classList.add('active');
+        $popup.classList.add('active');
+    });
+
+    function closeTapPopup() {
+        $overlay.classList.remove('active');
+        $popup.classList.remove('active');
+    };
+
+    $overlay.addEventListener('click', closeTapPopup);
+});
